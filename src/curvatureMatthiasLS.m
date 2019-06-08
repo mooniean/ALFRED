dffba01ded296adc46cf02381790ebc6c99f87c6
Nuno Miguel Nobre <nunomiguel.nobre@postgrad.manchester.ac.uk>
2019-06-08 19:05:02 +0100

    [Matthias/LS] Make code simpler/more consistent

--- src/curvatureMatthiasLS.m
+++ src/curvatureMatthiasLS.m
@@ -22,7 +22,7 @@ dydt=matlabFunction(diff(sym(yt),1));
 d2ydt2=matlabFunction(diff(sym(yt),2));
 
 kdldt= @(t) abs(dxdt(t).*d2ydt2(t)-dydt(t).*d2xdt2(t))./(dxdt(t).^2+dydt(t).^2);
-dldt = @(t) (dxdt(t).^2+dydt(t).^2).^(1/2);
+dldt = @(t) sqrt(dxdt(t).^2+dydt(t).^2);
 k = @(t) kdldt(t)./dldt(t);
 dldtk = @(t) dldt(t)./k(t);
 
@@ -34,7 +34,7 @@ radiusDensity=matlabFunction(sym(dldt)/diff(sym(k),1)*...
 
 % fp-precision digitization
 
-dtP=1e-4; tP=0:dtP:tMax; % the spacing affects what's the right p... with 1e-5 you should use p=0.(9) - some nines...
+dtP=1e-3; tP=0:dtP:tMax; % the spacing affects what's the right p... with 1e-5 you should use p=0.(9) - some nines...
 xP=xt(tP);
 yP=yt(tP);
 rP=1./k(tP);
@@ -46,10 +46,12 @@ rP=1./k(tP);
 minxP=min(xP); maxxP=max(xP);
 minyP=min(yP); maxyP=max(yP);
 analyticSkel=zeros(round(maxxP-minxP)+offset,round(maxyP-minyP)+offset);
+
+colorX=round(xP-minxP)+1+offset/2;
+colorY=round(yP-minyP)+1+offset/2;
 for i=1:length(xP)
-    colorX=round(xP(i)-minxP)+1+offset/2;
-    colorY=round(yP(i)-minyP)+1+offset/2;
-    analyticSkel(colorX-1:colorX+1,colorY-1:colorY+1)=log10(rP(i));
+    analyticSkel(colorX(i)-1:colorX(i)+1,colorY(i)-1:colorY(i)+1)=...
+        log10(rP(i));
 end
 
 subplot(141)
@@ -62,26 +64,20 @@ cMin=min(min(analyticSkel));cMax=max(max(analyticSkel));
 
 % spline the fp-precision curve
 
-%for p=10.^[0:-.5:-6]
-p=1;%.9999999999;
-ppX = csaps(tP, xP.', p);
-ppY = csaps(tP, yP.', p);
+for p=1%10^-3.456%10.^[-0.001:-.2:-6]
+    pause(eps)
+%p=1e-3;%.9999999999;
+ppX = csaps(tP, xP, p);
+ppY = csaps(tP, yP, p);
 
-coeffvaluesX=ppX.coefs;
-coeffvaluesY=ppY.coefs;
+xtP=@(t) fnval(ppX,t);
+ytP=@(t) fnval(ppY,t);
 
-xtP=@(t) coeffvaluesX(floor(t/dtP)+1,1).*(t-dtP*floor(t/dtP)).^3+coeffvaluesX(floor(t/dtP)+1,2).*(t-dtP*floor(t/dtP)).^2+...
-    coeffvaluesX(floor(t/dtP)+1,3).*(t-dtP*floor(t/dtP))+coeffvaluesX(floor(t/dtP)+1,4);
-ytP=@(t) coeffvaluesY(floor(t/dtP)+1,1).*(t-dtP*floor(t/dtP)).^3+coeffvaluesY(floor(t/dtP)+1,2).*(t-dtP*floor(t/dtP)).^2+...
-    coeffvaluesY(floor(t/dtP)+1,3).*(t-dtP*floor(t/dtP))+coeffvaluesY(floor(t/dtP)+1,4);
+dxdtP=@(t) fnval(fnder(ppX),t);
+dydtP=@(t) fnval(fnder(ppY),t);
 
-dxdtP=@(t) 3*coeffvaluesX(floor(t/dtP)+1,1).*(t-dtP*floor(t/dtP)).^2+2*coeffvaluesX(floor(t/dtP)+1,2).*(t-dtP*floor(t/dtP))+...
-    coeffvaluesX(floor(t/dtP)+1,3);
-dydtP=@(t) 3*coeffvaluesY(floor(t/dtP)+1,1).*(t-dtP*floor(t/dtP)).^2+2*coeffvaluesY(floor(t/dtP)+1,2).*(t-dtP*floor(t/dtP))+...
-    coeffvaluesY(floor(t/dtP)+1,3);
-
-d2xdt2P=@(t) 6*coeffvaluesX(floor(t/dtP)+1,1).*(t-dtP*floor(t/dtP))+2*coeffvaluesX(floor(t/dtP)+1,2);
-d2ydt2P=@(t) 6*coeffvaluesY(floor(t/dtP)+1,1).*(t-dtP*floor(t/dtP))+2*coeffvaluesY(floor(t/dtP)+1,2);
+d2xdt2P=@(t) fnval(fnder(fnder(ppX)),t);
+d2ydt2P=@(t) fnval(fnder(fnder(ppY)),t);
 
 kdldtP=@(t) abs(dydtP(t).*d2xdt2P(t)-d2ydt2P(t).*dxdtP(t))./(dxdtP(t).^2+dydtP(t).^2);
 dldtP=@(t) sqrt(dxdtP(t).^2+dydtP(t).^2);
@@ -95,7 +91,7 @@ radiusDensitySpline=@(t) (dldtP(t)./gradient(kP(t)).*kP(t).^2)...
 
 % spline of the fp-precison curve's skeleton
 
-tS=(0:dtP:tMax-5*dtP).'; % cheating: completely empirical
+tS=tP;
 xS=xtP(tS);
 yS=ytP(tS);
 rS=1./kP(tS);
@@ -103,11 +99,14 @@ rS=1./kP(tS);
 minxS=min(xS);
 minyS=min(yS);
 splineAnalyticSkel=zeros(size(analyticSkel));
-for i=1:length(xS)
-    colorX=round(xS(i)-minxS)+1+offset/2;
-    colorY=round(yS(i)-minyS)+1+offset/2;
-    splineAnalyticSkel(colorX-1:colorX+1,colorY-1:colorY+1)=log10(rS(i));
+
+colorX=round(xS-minxS)+1+offset/2;
+colorY=round(yS-minyS)+1+offset/2;
+for i=1:length(xS)  
+    splineAnalyticSkel(colorX(i)-1:colorX(i)+1,colorY(i)-1:colorY(i)+1)=...
+        log10(rS(i));
 end
+%assert(isequal(size(analyticSkel),size(splineAnalyticSkel)));
 
 subplot(142),cla
 imagesc(splineAnalyticSkel,[cMin cMax]); daspect([1 1 1]); colormap(gca,'jet'); %cheating: fixing the colormap to that of the analytic curve
@@ -139,45 +138,45 @@ skeleton=bwmorph(skeleton,'skel','Inf');
 % [dTdt, dsdt, xt, yt, ~] = curvatureGauss(x,y,sigma);
 % 
 % dt=1;
-% xt{1}=round(xt{1});
-% yt{1}=round(yt{1});
+% xt=round(xt{1});
+% yt=round(yt{1});
 
 % give this to Spline fit function
 
 p=2e-5;
 [dTdt, dsdt, xt, yt, dsdT] = curvatureSpline(x,y,p);
 
-dt=1e-2;t=20:dt:length(x)-20; % cheating: completely empirical
-dTdt{1}=dTdt{1}(t.');
-dsdt{1}=dsdt{1}(t.');
-xt{1}=round(xt{1}(t.'));
-yt{1}=round(yt{1}(t.'));
+dt=dtP;t=20:dt:length(x)-20; % cheating: completely empirical
+dTdt=dTdt{1}(t.');
+dsdt=dsdt{1}(t.');
+xt=round(xt{1}(t.'));
+yt=round(yt{1}(t.'));
 
 %%%%%----------%%%%%
 
 % colorize curvature on the logarithmic spiral per constant-length-sector
 sections=350;
 
-circumference=dt*trapz(dsdt{1});
+circumference=dt*trapz(dsdt);
 secLen=circumference/sections;
 
-deltaS=dt*cumtrapz(dsdt{1});
+deltaS=dt*cumtrapz(dsdt);
 breakpoints=ones(1,sections+1);
 for i=1:sections-1; [~,breakpoints(i+1)]=min(abs(deltaS-secLen*i)); end
-breakpoints(end)=length(dsdt{1});
+breakpoints(end)=length(dsdt);
 
-meanGaussRadius=trapz(abs(dsdt{1}./dTdt{1}.*dsdt{1}))/trapz(dsdt{1})
+meanGaussRadius=trapz(abs(dsdt./dTdt.*dsdt))/trapz(dsdt)
 meanGaussRadii=zeros(1,sections);
 actualSectionLens=zeros(1,sections);
 coloredSkeleton=zeros(size(analyticSkel));
 for i=1:sections
-    meanGaussRadii(i)=trapz(abs(dsdt{1}(breakpoints(i):breakpoints(i+1))...
-        ./dTdt{1}(breakpoints(i):breakpoints(i+1))...
-        .*dsdt{1}(breakpoints(i):breakpoints(i+1))))...
-        /trapz(dsdt{1}(breakpoints(i):breakpoints(i+1)));
-    actualSectionLens(i)=dt*trapz(dsdt{1}(breakpoints(i):breakpoints(i+1)));
+    meanGaussRadii(i)=trapz(abs(dsdt(breakpoints(i):breakpoints(i+1))...
+        ./dTdt(breakpoints(i):breakpoints(i+1))...
+        .*dsdt(breakpoints(i):breakpoints(i+1))))...
+        /trapz(dsdt(breakpoints(i):breakpoints(i+1)));
+    actualSectionLens(i)=dt*trapz(dsdt(breakpoints(i):breakpoints(i+1)));
     for j=breakpoints(i):breakpoints(i+1)-1
-        colorX=xt{1}(j);colorY=yt{1}(j);
+        colorX=xt(j);colorY=yt(j);
         coloredSkeleton(colorX-1:colorX+1,colorY-1:colorY+1)=log10(abs(meanGaussRadii(i)));
     end
 end
@@ -243,4 +242,4 @@ legend(plots([6:-1:1,7]),...
     })
 xlim([0,250]);ylim([0 yLim]);
 %if p>.9; gif(['lSpiral_Spline_',int2str(sections),'.gif'],'frame',gcf,'DelayTime',2); else; gif; end
-%end
+end

6158c5127a27a3b52ef69d2b23de8a1179874ec4
Beatriz Costa Gomes <bcostagomes@turing.ac.uk>
2020-02-17 19:00:27 +0000

    [Calculations] Updated the Hough Transform. Still has bug but its improving

--- src/calculationFunction.m
+++ src/calculationFunction.m
@@ -250,23 +250,24 @@ if string(saveAnswer(1))=="Yes"
             k=1;
             while k<=sizeStraightLines
                 try
-                    if (straightLines(k).rho == straightLines(k+1).rho) && (straightLines(k).theta == straightLines(k+1).theta)
-                        %                         temp = handles.finalROIs(i).straightlineDistances(k);
-                        values = [k];
-                        while (k+1<=sizeStraightLines && straightLines(k).rho == straightLines(k+1).rho) && (straightLines(k).theta == straightLines(k+1).theta)
-                            %                         distances = [distances finalROIs(i).straightlineDistances(k)+finalROIs(i).straightlineDistances(k+1)];
-                            %                             temp = temp + handles.finalROIs(i).straightlineDistances(k+1);
-                            
-                            k=k+1;
-                            values = [values k];
-                        end
-                        coordinates = [];
-                        for tempindex = values
-                            coordinates = [coordinates; straightLines(tempindex).point1;straightLines(tempindex).point2];
-                        end
-                        temp = checkDistance(coordinates(1,1),coordinates(end,1),coordinates(1,2),coordinates(end,2));
-                        distances = [distances temp];
-                    else
+%                     if (straightLines(k).rho == straightLines(k+1).rho) && (straightLines(k).theta == straightLines(k+1).theta)
+%                         %                         temp = handles.finalROIs(i).straightlineDistances(k);
+%                         values = [k];
+%                         while (k+1<=sizeStraightLines && straightLines(k).rho == straightLines(k+1).rho) && (straightLines(k).theta == straightLines(k+1).theta)
+%                             %                         distances = [distances finalROIs(i).straightlineDistances(k)+finalROIs(i).straightlineDistances(k+1)];
+%                             %                             temp = temp + handles.finalROIs(i).straightlineDistances(k+1);
+%                             
+%                             k=k+1;
+%                             values = [values k];
+%                         end
+%                         coordinates = [];
+%                         for tempindex = values
+%                             coordinates = [coordinates; straightLines(tempindex).point1;straightLines(tempindex).point2];
+%                         end
+%                         temp = checkDistance(coordinates(1,1),coordinates(end,1),coordinates(1,2),coordinates(end,2));
+%                         distances = [distances temp];
+                    
+                    if (handles.finalROIs(i).straightlineDistances(k)/handles.finalROIs(i).length > 0.01)
                         distances = [distances handles.finalROIs(i).straightlineDistances(k)];
                     end
                 catch
--- test.m
+++ test.m
@@ -1,9 +1,10 @@
 k=1;
 distances=[];
+sizeStraightLines=size(straightLines,2);
 while k<=sizeStraightLines
     try
         if (straightLines(k).rho == straightLines(k+1).rho) && (straightLines(k).theta == straightLines(k+1).theta)
-            temp = tempROIs(9).straightlineDistances(k);
+            temp = tempROIs(5).straightlineDistances(k);
             values = [k];
             while (k+1<=sizeStraightLines && straightLines(k).rho == straightLines(k+1).rho) && (straightLines(k).theta == straightLines(k+1).theta)
                 %                         distances = [distances finalROIs(i).straightlineDistances(k)+finalROIs(i).straightlineDistances(k+1)];
@@ -19,10 +20,10 @@ while k<=sizeStraightLines
             temp = checkDistance(coordinates(1,1),coordinates(end,1),coordinates(1,2),coordinates(end,2));
             distances = [distances temp];
         else
-            distances = [distances tempROIs(9).straightlineDistances(k)];
+            distances = [distances tempROIs(5).straightlineDistances(k)];
         end
     catch
-        distances = [distances tempROIs(9).straightlineDistances(k)];
+        distances = [distances tempROIs(5).straightlineDistances(k)];
     end
     k=k+1;
 end
